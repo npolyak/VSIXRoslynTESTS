b0f379cc7965b32fb0839fb08ccf15fcadb0d4f6
npolyak <nick.polyak@verizon.net>
2017-11-16 13:25:33 -0500

    checkpoint

---
+++ NP.Paradigms.Attrs/PostNotifiablePropertyAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.Paradigms.Attrs
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    class PostNotifiablePropertyAttribute : Attribute
+    {
+    }
+}
