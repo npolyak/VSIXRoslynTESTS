7767b462fb3cdf7b135e404ce3d72fbcf5f6a0d7
npolyak <nick.polyak@verizon.net>
2017-11-16 14:24:11 -0500

    removed the test project from the original solution. created a stand alone solution for that project.

--- PropertyExpansionTest/Program.cs
+++ PropertyExpansionTest/Program.cs
@@ -10,6 +10,8 @@ namespace PropertyExpansionTest
     {
         static void Main(string[] args)
         {
+            const string pathToSolution = @"..\..\..\..\EnumTest\EnumExtension.sln";
+            const string projectName = "EnumExtension";
         }
     }
 }
